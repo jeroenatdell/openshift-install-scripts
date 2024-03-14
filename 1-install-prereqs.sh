6876798ac8784ec9b1f2c1fed7605106df657075
Jeroen Huisman <125976358+jeroenatdell@users.noreply.github.com>
2024-03-14 18:24:06 +0000

    Update 1-install-prereqs.sh

--- 1-install-prereqs.sh
+++ 1-install-prereqs.sh
@@ -41,9 +41,9 @@ echo ""
 echo Downloading remaining files...
 for TAR_FILE in "${OCP_FILES[@]}"
 do
-	FILE_DOWNLOAD="${DOWNLOAD_MIRROR}/ocp/${OCP_RELEASE}/${TAR_FILE}"
+	FILE_DOWNLOAD="${DOWNLOAD_MIRROR}/ocp/${OCP_RELEASE}/${TAR_FILE}.tar.gz"
 	DOWNLOADED_FILE="${DOWNLOAD_LOCATION}/${TAR_FILE}"
-	EXPECTED_SHA=$(curl -s ${DOWNLOAD_MIRROR}/ocp/${OCP_RELEASE}/sha256sum.txt | grep ${TAR_FILE} | awk '{print $1}')
+	EXPECTED_SHA=$(curl -s ${DOWNLOAD_MIRROR}/ocp/${OCP_RELEASE}/sha256sum.txt | grep ${TAR_FILE}-${OCP_RELEASE}.tar.gz | awk '{print $1}')
 
 	echo -n -e "\rDownloading	${TAR_FILE}			[	In Progress	]"
 	if [ ! -f  ${DOWNLOADED_FILE} ]; then
